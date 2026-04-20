2fdec68ccab252aaf034f0ce890c1875ff633dc2
Delweng <delweng@gmail.com>
2026-04-21 01:04:41 +0800

    Add Event Decoder page and adjust navigation position (#16)
    * feat: add Event Decoder page with topics/data input and abi_server fallback decoding
    
    Signed-off-by: Delweng <delweng@gmail.com>
    
    * fix(nav): move Event Decoder next to Tx Decoder
    
    Signed-off-by: Delweng <delweng@gmail.com>
    
    ---------
    
    Signed-off-by: Delweng <delweng@gmail.com>

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -20,6 +20,12 @@ export default function Nav() {
             Tx Decoder
           </Link>
           <Link
+            href="/event-decoder"
+            className={`${styles.link} ${pathname === '/event-decoder' ? styles.active : ''}`}
+          >
+            Event Decoder
+          </Link>
+          <Link
             href="/contract-caller"
             className={`${styles.link} ${pathname === '/contract-caller' ? styles.active : ''}`}
           >
